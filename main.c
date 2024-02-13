fe1a73dbbf3f5fc1595c219702ef198fece3a2e6
emptyboy84 <92681205+emptyboy84@users.noreply.github.com>
2024-02-13 17:44:52 +0900

    mustPick

--- main.c
+++ main.c
@@ -9,36 +9,5 @@ int main(){
         if(result<num[i])
             result=num[i];
     }
- printf("%d",result) ;  
+printf("%d",result);
 }
-
-
-// // int main(){
-// //     int arr[10]={28,56,45,61,50,20,91,11,21,27};
-// //     int result=0;
-// //     for(int i=0;i<10;i+=2)
-// //     {
-// //         result+=arr[i];
-// //     }
-// //     printf("%d",result);
-
-// // }
-
-// // // int main(){
-// //     int a[4];
-// //     int s=0;
-// //     int i;
-// //     for(i=0;i<4;i++){
-// //         printf("%d번째값을입력하시오:",i+1);
-// //         scanf("%d",&a[i]);
-
-// //     }
-
-// //     for (i=3;i>0;i--)s+=a[i];
-// //     printf("%d",s);
-// // }
-
-// int main(){
-//     static int b[9]={1,2,3};
-//     printf("%d",b[5]);
-// }
